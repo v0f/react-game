68d2f6fc170de7ccd796739462a8ef4f78d357ed
vof <vof1111@gmail.com>
2021-03-05 05:30:03 +0300

    feat: style components

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import Button from '@material-ui/core/Button';
+import AutorenewIcon from '@material-ui/icons/Autorenew';
 import Board from '../Board/Board';
 import { getEmptyBoard, generateShips } from '../../logic';
 import './Game.css';
@@ -16,7 +18,15 @@ function Game() {
         <Board className="player1" boardState={state} />
         {/* <Board className="player2" /> */}
       </div>
-      <button type="button" onClick={handleClick}>generate</button>
+      <Button
+        className="genShipsButton"
+        variant="contained"
+        size="medium"
+        endIcon={<AutorenewIcon />}
+        onClick={handleClick}
+      >
+        расставить корабли
+      </Button>
     </div>
   );
 }
