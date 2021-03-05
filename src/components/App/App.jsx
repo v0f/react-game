8ddbe4267d02521f2390ac8e1ba6d1f60ac347e0
vof <vof1111@gmail.com>
2021-03-05 17:41:01 +0300

    feat: add header and footer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Game from '../Game/Game';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Game />
+      <Footer />
     </div>
   );
 }
